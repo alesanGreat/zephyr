bab1b49dae3030c21d9d06b8d060406ab8a8620b
Alejandro Sánchez <alesangreat@gmail.com>
2026-09-17 16:25:44 -0500

    ci: validate PR 117079 FFF review fix

--- tests/drivers/virtio/pci_init_irq/src/main.c
+++ tests/drivers/virtio/pci_init_irq/src/main.c
@@ -5,31 +5,27 @@
  */
 
 #include <zephyr/device.h>
+#include <zephyr/fff.h>
 #include <zephyr/ztest.h>
 
 #define TEST_NODE DT_NODELABEL(virtio_pci_test)
 
-static unsigned int irq_enable_count;
-
-void __wrap_arch_irq_enable(unsigned int irq)
-{
-	ARG_UNUSED(irq);
-	irq_enable_count++;
-}
+DEFINE_FFF_GLOBALS;
+FAKE_VOID_FUNC(arch_irq_enable, unsigned int);
 
 ZTEST(virtio_pci_init_irq, test_failed_init_does_not_enable_irq)
 {
 	const struct device *dev = DEVICE_DT_GET(TEST_NODE);
 	int ret;
 
-	irq_enable_count = 0U;
+	RESET_FAKE(arch_irq_enable);
 	zassert_false(device_is_ready(dev), "deferred VirtIO PCI device unexpectedly ready");
 	zassert_not_null(dev->ops.init, "VirtIO PCI device has no init callback");
 
 	ret = dev->ops.init(dev);
 
 	zassert_not_equal(ret, 0, "VirtIO PCI transport unexpectedly initialized");
-	zassert_equal(irq_enable_count, 0U,
+	zassert_equal(arch_irq_enable_fake.call_count, 0U,
 		      "VirtIO PCI IRQ enabled despite failed transport initialization");
 }
 
